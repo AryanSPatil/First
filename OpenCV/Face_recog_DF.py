54d713168391e3e36f9a2858ada56ca9775c1a7b
AryanSPatil <aryanpatil7350@gmail.com>
2024-03-14 21:56:09 +0530

    third commit with CODE

--- OpenCV/Face_recog_DF.py
+++ OpenCV/Face_recog_DF.py
@@ -1,20 +1,21 @@
-# Threading is used for
+# Threading is used for simultaneous processing.
 import threading
-
+# Importing libraries like OpenCV and deepface.
 import cv2
 from deepface import DeepFace
-
+#Sets which camera captures the video, in this case its camera 0,
+#which means the first camera it detects
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
+#Provides Frame size for window
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+#Counter to check frames in interval of 30(to reduce load)
 counter = 0
-
+#Boolean to facematch
 face_match = False
-
+#Providing a reference image
 reference_img = cv2.imread("IMG_20210521_142909.jpg")
-
+#Deepface verifies the frame for the image
 def check_face(frame):
     global face_match
     try:
@@ -25,7 +26,7 @@ def check_face(frame):
             face_match = False
     except ValueError:
         face_match = False
-
+#Reading the frame and displaying text on screen
 while True:
     ret, frame = cap.read()
 
@@ -49,5 +50,5 @@ while True:
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
-
+#Clears all windows
 cv2.destroyAllWindows()
